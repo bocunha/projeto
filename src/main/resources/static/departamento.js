016fb06b6029b7e638a681bd100a6f81a2bf8c12
bocunha <bocunha@hotmail.com>
2020-07-24 09:45:07 -0300

    add departamento

--- src/main/resources/static/departamento.js
+++ src/main/resources/static/departamento.js
@@ -1,10 +1,13 @@
-var templateFoto = `<img src="**FOTO**" width="100%">`;
-var templateInfo = `<strong>Nome:</strong> **NOME** <br>
+var templateLinha = `<tr><td><img src="**FOTO**" width="100%"></td>
+                    <td><strong>Nome:</strong> **NOME** <br>
                     <strong>RACF:</strong> **RACF** <br>
                     <strong>EMAIL:</strong> **EMAIL** <br>
                     <strong>DEPARTAMENTO:</strong> **DEPTO** <br>
-                    <strong>UNIDADE:</strong> **UNIDADE**
+                    <strong>UNIDADE:</strong> **UNIDADE** </td> </tr>
                    `;
+
+
+
 
 
 var url = "https://isidronianos.herokuapp.com"
@@ -22,28 +25,29 @@ function preencheInfo() {
 
         console.log("Id Depto = " + idDepto);
 
-        fetch("http://localhost:8088/departamentos/" + idDepto)
+        fetch(url+ "/departamentos/" + idDepto)
             .then(res => res.json())
             .then(res => PreencheUser(res));
 
         function PreencheUser(res) {
-            var UserDepartamento = "";
-            for (i = 0; i < res.length; i++) {
-                console.log(res);
-                var objUser = res[i];
-                var strFoto = templateFoto.replace("**FOTO**", objUser.linkFoto);
+            var tabela = "";
+            console.log(Object.keys(res.usarUsuario).length);
+            console.log(res);
+        
+            var objUser = JSON.parse(user);  // aqui vou converter a STRING armazenada para um objeto
 
-                document.getElementById("fotoUser").innerHTML = strFoto;
-
-                var strInfo = templateInfo.replace("**NOME**", objUser.nome)
-                    .replace("**RACF**", objUser.racf)
-                    .replace("**EMAIL**", objUser.email)
+            for (i = 0; i < Object.keys(res).length; i++) {
+                var User = res.usarUsuario[i];
+                console.log(User);
+                var strLinha = templateLinha.replace("**NOME**", User.nome)
+                    .replace("**RACF**", User.racf)
+                    .replace("**EMAIL**", User.email)
                     .replace("**DEPTO**", objUser.depto.nomedpt)
                     .replace("**UNIDADE**", objUser.depto.unidade)
-                    .replace("**ID**", objUser.depto.id);
-                document.getElementById("infoUser").innerHTML = strInfo;
-                console.log(objUser);
+                    console.log(strLinha);
+                tabela = tabela + strLinha;
             }
+            document.getElementById("relatorio").innerHTML = tabela;
         }
     }
 }
